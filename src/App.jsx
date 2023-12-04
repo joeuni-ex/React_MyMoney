415a82a94da200dd444804b80b754c7563a81875
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-04 09:28:25 +0900

    Navbar 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
       <div className="App">
         <BrowserRouter>
+          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/" element={<Login />} />
-            <Route path="/" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </BrowserRouter>
       </div>
