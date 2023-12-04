220451bbe58da5742722deed498a821c365705a8
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-04 12:07:13 +0900

    .env 파일 사용으로 api키 가져오기

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.VITE_API_KEY,
-  authDomain: import.meta.VITE_AUTH_DOMAIN,
-  projectId: import.meta.VITE_PROJECTID,
-  storageBucket: import.meta.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.VITE_MESSAGEING_SENDERID,
-  appId: import.meta.VITE_APPID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGEING_SENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 //본인 계정의 키 등 설정값을 가지고 초기화
