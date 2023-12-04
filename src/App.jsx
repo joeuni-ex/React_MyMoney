ff2b9f8e3398d71f91fdd7a5ad69a77d1a52a3e0
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-04 14:57:56 +0900

    인증 후 실행

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./Components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  //파이어 베이스 인증확인을 한 후 앱을 사용가능
+  const { authIsReady } = useAuthContext();
+
   return (
     <>
       <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </BrowserRouter>
+        {authIsReady && (
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </BrowserRouter>
+        )}
       </div>
     </>
   );
