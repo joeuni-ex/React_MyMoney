21631b57172383bda21c28373878058fb785df18
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-04 15:10:59 +0900

    TransactionForm 컴포넌트 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,15 @@
+import TransactionForm from "../../Components/TransactionForm";
 import styles from "./Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>거래 내역</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
