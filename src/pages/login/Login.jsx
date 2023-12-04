c9948810435ab6ffa3d3ebbcb91d38fcbff21ad1
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-04 09:39:49 +0900

    Login 페이지 form 추가 및 css적용

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import styles from "./Login.module.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState(""); //이메일
+  const [password, setPassword] = useState(""); //비밀번호
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["login-form"]}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">로그인</button>
+    </form>
+  );
 };
 
 export default Login;
