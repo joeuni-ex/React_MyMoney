aa19641f24da17f8b46e6e045415d6ae182b7582
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-04 14:32:14 +0900

    인증(로그인)된 유저만 로그아웃 노출

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,12 @@
 //CSS 모듈을 사용하면 CSS클래스가 다른 컴포넌트에도 적용되는것을 방지함
+import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  //유저 정보 가져오기
+  const { user } = useAuthContext();
   //로그아웃 메서드
   const { logout } = useLogout();
   return (
@@ -12,17 +15,26 @@ const Navbar = () => {
         <li className={styles.title}>
           <Link to="/">myMoney</Link>
         </li>
-        <li>
-          <Link to="/login">로그인</Link>
-        </li>
-        <li>
-          <Link to="/signup">가입</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            로그아웃
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">로그인</Link>
+            </li>
+            <li>
+              <Link to="/signup">가입</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>안녕하세요, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                로그아웃
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
