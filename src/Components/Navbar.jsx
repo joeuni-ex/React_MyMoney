275620e3bb70c1ae2ab9947d6a03c04e969d14f7
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-04 09:39:27 +0900

    module.css 부가 설명

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+//CSS 모듈을 사용하면 CSS클래스가 다른 컴포넌트에도 적용되는것을 방지함
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 
@@ -5,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className="{styles.title}">myMoney</li>
+        <li className={styles.title}>myMoney</li>
         <li>
           <Link to="/login">로그인</Link>
         </li>
