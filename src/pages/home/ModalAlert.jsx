1831a914fec68f32d6e9890e8dcc337d8248410f
joeuni <dmsdl8917@gmail.com>
2023-12-06 18:34:02 +0900

    CSS수정

--- src/pages/home/ModalAlert.jsx
+++ src/pages/home/ModalAlert.jsx
@@ -1,7 +1,7 @@
 const ModalAlert = ({ onModalAlert, item }) => {
   return (
-    <div className="h-screen w-full fixed left-0 top-0 flex  justify-center items-center bg-black bg-opacity-10 text-center">
-      <div className=" rounded w-10/12 md:w-1/3 h-max bg-yellow-100">
+    <div className="h-full w-screen fixed left-0 top-0 flex  justify-center items-center bg-black bg-opacity-10 text-center">
+      <div className=" rounded w-6/12 md:w-1/3 h-max bg-yellow-100">
         <div className="border-b-2 border-orange-400 px-4 py-2 flex justify-between items-center">
           <h3 className="font-extrabold">상세보기</h3>
           <span onClick={onModalAlert}>
